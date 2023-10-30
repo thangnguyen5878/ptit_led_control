44b89fc2522544e90373a7c19f5a2882989cec31
thang5878 <thang5878@gmail.com>
2023-10-30 22:15:23 +0700

    refactor for one led

--- LedControl.ino
+++ LedControl.ino
@@ -24,7 +24,7 @@
 #define BLYNK_TEMPLATE_ID "TMPL6lwrbAEGk"
 #define BLYNK_TEMPLATE_NAME "LedControl2"
 
-#define BLYNK_FIRMWARE_VERSION      "0.1.0"
+#define BLYNK_FIRMWARE_VERSION "0.1.0"
 
 #define BLYNK_PRINT Serial
 //#define BLYNK_DEBUG
@@ -37,251 +37,161 @@
 //#define USE_WITTY_CLOUD_BOARD
 //#define USE_WEMOS_D1_MINI
 
+#define LED_PIN D0  // Pin for the LED
+
+#define LED_CONTROL_PIN V0  // Widget for LED control
+#define LED_TIMER V2        // Widget for LED timer
+
 #include "BlynkEdgent.h"
 #include <TimeLib.h>
 
 BlynkTimer timer;
 
-bool led_set[2];
-long timer_start_set[2] = {0xFFFF, 0xFFFF};
-long timer_stop_set[2] = {0xFFFF, 0xFFFF};
-unsigned char weekday_set[2];
+bool led;
+long timer_start = 0xFFFF; // thời điểm đèn bắt đầu sáng (tính bằng giây)
+long timer_stop = 0xFFFF;   // thời điểm đèn ngừng sáng (tính bằng giây)
+unsigned char weekday_option;
 
-long rtc_sec;
+long rtc_sec; // thời điểm hiện tại (tính bằng giây)
 unsigned char day_of_week;
 
-bool led_status[2];
-bool update_blynk_status[2];
-bool led_timer_on_set[2];
+bool led_status; // trạng thái của led và nút nhấn ảo (0: tắt, 1: bật)
+bool update_blynk_status;
+bool led_timer_on_set;
 
 // #########################################################################################################
 // LED 1
-BLYNK_WRITE(V0)
-{
+BLYNK_WRITE(LED_CONTROL_PIN) {
   int val = param.asInt();
-
-  if ( led_timer_on_set[0] == 0 )
-    led_set[0] = val;
+  // Nếu không trong thời gian của timer, có thể dùng nút nhấn để điều khiển led
+  if (led_timer_on_set == 0)
+    led = val; 
+  // khi đang trong thời gian của timer thì không thể nhấn nút ảo để điều khiển led
   else
-    update_blynk_status[0] = 1;
-}
-
-// #########################################################################################################
-// LED 2
-BLYNK_WRITE(V1)
-{
-  int val = param.asInt();
-
-  if ( led_timer_on_set[1] == 0 )
-    led_set[1] = val;
-  else
-    update_blynk_status[1] = 1;  
+    update_blynk_status = 1; // cập nhật trạng thái của led cho nút ảo
 }
 
 // #########################################################################################################
 // Timer 1
-BLYNK_WRITE(V2) 
-{
+BLYNK_WRITE(LED_TIMER) {
   unsigned char week_day;
- 
+
   TimeInputParam t(param);
 
-  if (t.hasStartTime() && t.hasStopTime() ) 
-  {
-    timer_start_set[0] = (t.getStartHour() * 60 * 60) + (t.getStartMinute() * 60) + t.getStartSecond();
-    timer_stop_set[0] = (t.getStopHour() * 60 * 60) + (t.getStopMinute() * 60) + t.getStopSecond();
-    
-    Serial.println(String("Start Time: ") +
-                   t.getStartHour() + ":" +
-                   t.getStartMinute() + ":" +
-                   t.getStartSecond());
-                   
-    Serial.println(String("Stop Time: ") +
-                   t.getStopHour() + ":" +
-                   t.getStopMinute() + ":" +
-                   t.getStopSecond());
-                   
-    for (int i = 1; i <= 7; i++) 
-    {
-      if (t.isWeekdaySelected(i)) 
-      {
-        week_day |= (0x01 << (i-1));
-        Serial.println(String("Day ") + i + " is selected");
-      }
-      else
-      {
-        week_day &= (~(0x01 << (i-1)));
-      }
-    } 
+  if (t.hasStartTime() && t.hasStopTime()) {
+    timer_start = (t.getStartHour() * 60 * 60) + (t.getStartMinute() * 60) + t.getStartSecond();
+    timer_stop = (t.getStopHour() * 60 * 60) + (t.getStopMinute() * 60) + t.getStopSecond();
 
-    weekday_set[0] = week_day;
-  }
-  else
-  {
-    timer_start_set[0] = 0xFFFF;
-    timer_stop_set[0] = 0xFFFF;
+    Serial.println(String("Start Time: ") + t.getStartHour() + ":" + t.getStartMinute() + ":" + t.getStartSecond());
+
+    Serial.println(String("Stop Time: ") + t.getStopHour() + ":" + t.getStopMinute() + ":" + t.getStopSecond());
+
+    for (int i = 1; i <= 7; i++) {
+      if (t.isWeekdaySelected(i)) {
+        week_day |= (0x01 << (i - 1));
+        Serial.println(String("Day ") + i + " is selected");
+      } else {
+        week_day &= (~(0x01 << (i - 1)));
+      }
+    }
+
+    weekday_option = week_day;
+  } else {
+    timer_start = 0xFFFF;
+    timer_stop = 0xFFFF;
   }
 }
 
 // #########################################################################################################
-// Timer 2
-BLYNK_WRITE(V3) 
-{
-unsigned char week_day;
- 
-  TimeInputParam t(param);
+BLYNK_WRITE(InternalPinRTC) {
+  const unsigned long DEFAULT_TIME = 1357041600;  // Mốc thời gian mặc định 1/1/2013
+  unsigned long blynkTime = param.asLong();
 
-  if (t.hasStartTime() && t.hasStopTime() ) 
-  {
-    timer_start_set[1] = (t.getStartHour() * 60 * 60) + (t.getStartMinute() * 60) + t.getStartSecond();
-    timer_stop_set[1] = (t.getStopHour() * 60 * 60) + (t.getStopMinute() * 60) + t.getStopSecond();
-    
-    Serial.println(String("Start Time: ") +
-                   t.getStartHour() + ":" +
-                   t.getStartMinute() + ":" +
-                   t.getStartSecond());
-                   
-    Serial.println(String("Stop Time: ") +
-                   t.getStopHour() + ":" +
-                   t.getStopMinute() + ":" +
-                   t.getStopSecond());
-                   
-    for (int i = 1; i <= 7; i++) 
-    {
-      if (t.isWeekdaySelected(i)) 
-      {
-        week_day |= (0x01 << (i-1));
-        Serial.println(String("Day ") + i + " is selected");
-      }
-      else
-      {
-        week_day &= (~(0x01 << (i-1)));
-      }
-    } 
-
-    weekday_set[1] = week_day;
-  }
-  else
-  {
-    timer_start_set[1] = 0xFFFF;
-    timer_stop_set[1] = 0xFFFF;
-  }
-}
-
-// #########################################################################################################
-BLYNK_WRITE(InternalPinRTC) 
-{
-  const unsigned long DEFAULT_TIME = 1357041600; // Jan 1 2013
-  unsigned long blynkTime = param.asLong(); 
-  
-  if (blynkTime >= DEFAULT_TIME) 
-  {
+  if (blynkTime >= DEFAULT_TIME) {
     setTime(blynkTime);
 
     day_of_week = weekday();
-  
-    if ( day_of_week == 1 )
+
+    if (day_of_week == 1)
       day_of_week = 7;
     else
-      day_of_week -= 1; 
-    
-    rtc_sec = (hour()*60*60) + (minute()*60) + second();
-   
+      day_of_week -= 1;
+
+    rtc_sec = (hour() * 60 * 60) + (minute() * 60) + second();
+
     Serial.println(blynkTime);
     Serial.println(String("RTC Server: ") + hour() + ":" + minute() + ":" + second());
-    Serial.println(String("Day of Week: ") + weekday()); 
+    Serial.println(String("Day of Week: ") + weekday());
   }
 }
 
 // #########################################################################################################
-BLYNK_CONNECTED() 
-{
-  Blynk.sendInternal("rtc", "sync"); 
+BLYNK_CONNECTED() {
+  Blynk.sendInternal("rtc", "sync");
 }
 
 // #########################################################################################################
-void checkTime() 
-{
-  Blynk.sendInternal("rtc", "sync"); 
+void checkTime() {
+  Blynk.sendInternal("rtc", "sync");
 }
 
 // #########################################################################################################
-void led_mng()
-{
-  bool time_set_overflow;
-  bool led_status_buf[2];
-  
-  for (int i=0; i<2; i++)
-  {
-    led_status_buf[i] = led_status[i];
-    time_set_overflow = 0;
-    
-    if ( timer_start_set[i] != 0xFFFF && timer_stop_set[i] != 0xFFFF)
-    {
-      if ( timer_stop_set[i] < timer_start_set[i] ) time_set_overflow = 1;
+void led_mng() {
+  bool old_led_status;
+  old_led_status = led_status;
 
-      if ((((time_set_overflow == 0 && (rtc_sec >= timer_start_set[i]) && (rtc_sec < timer_stop_set[i])) ||
-        (time_set_overflow  && ((rtc_sec >= timer_start_set[i]) || (rtc_sec < timer_stop_set[i])))) && 
-        (weekday_set[i] == 0x00 || (weekday_set[i] & (0x01 << (day_of_week - 1) )))) )
-        {
-          led_timer_on_set[i] = 1;
-        }
-        else
-          led_timer_on_set[i] = 0;
-    }
-    else
-      led_timer_on_set[i] = 0;
 
-    if ( led_timer_on_set[i] )
-    {
-      led_status[i] = 1;
-      led_set[i] = 0;
-    }
-    else
-    {
-      led_status[i] = led_set[i];
-    }
+  if (timer_start != 0xFFFF && timer_stop != 0xFFFF) {
 
-    if ( led_status_buf[i] != led_status[i] )
-      update_blynk_status[i] = 1;  
+    if (
+      (
+        (
+          ((timer_start <= timer_stop) && (rtc_sec >= timer_start) && (rtc_sec < timer_stop)) 
+          || ((timer_start >= timer_stop) && ((rtc_sec >= timer_start) || (rtc_sec < timer_stop)))
+        ) && (weekday_option == 0x00 || (weekday_option & (0x01 << (day_of_week - 1)))))) 
+    {
+      led_timer_on_set = 1;
+    } else
+      led_timer_on_set = 0;
+  } else
+    led_timer_on_set = 0;
+
+  if (led_timer_on_set) {
+    led_status = 1;
+    led = 0;
+  } else {
+    led_status = led;
   }
-    // HARDWARE CONTROL
-  digitalWrite(D0, led_status[0]);  
-  digitalWrite(D1, led_status[1]);  
+
+  if (old_led_status != led_status)
+    update_blynk_status = 1;
+
+  // HARDWARE CONTROL
+  digitalWrite(LED_PIN, led_status); // thay đổi trạng thái Led
+}
+
+// cập nhật trạng thái nút nhấn áo tương ứng với trạng thái của led
+void blynk_update() {
+  if (update_blynk_status) {
+    Blynk.virtualWrite(LED_CONTROL_PIN, led_status); // thay đổi trạng thái nút nhấn ảo
+    update_blynk_status = 0; // đã update xong, chuyển về 0
+  }
 }
 
 // #########################################################################################################
-void blynk_update()
-{
-  if ( update_blynk_status[0] )
-  {
-      update_blynk_status[0] = 0;
-      Blynk.virtualWrite(V0, led_status[0]);
-  }  
-
-  if ( update_blynk_status[1] )
-  {
-      update_blynk_status[1] = 0;
-      Blynk.virtualWrite(V1, led_status[1]);
-  } 
-}
-
-// #########################################################################################################
-void setup()
-{
+void setup() {
   Serial.begin(115200);
   delay(100);
 
-  pinMode(D0, OUTPUT);
-  pinMode(D1, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+
 
   BlynkEdgent.begin();
   timer.setInterval(10000L, checkTime);
 }
 
 // #########################################################################################################
-void loop() 
-{
+void loop() {
   BlynkEdgent.run();
   timer.run();
   led_mng();
